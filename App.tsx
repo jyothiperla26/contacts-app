a661867293abd08f0a37865a839bbd000f63a8b7
jyothi_perla26 <jyothi.perla@everst.engineering>
2023-09-19 16:12:35 +0530

    The basic users sign-in intialized

---
+++ App.tsx
@@ -0,0 +1,18 @@
+/* eslint-disable prettier/prettier */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  Text,
+} from 'react-native';
+
+function App(): JSX.Element {
+
+  return (
+    <SafeAreaView>
+      <Text>Sign-in User</Text>
+    </SafeAreaView>
+  );
+}
+
+export default App;
