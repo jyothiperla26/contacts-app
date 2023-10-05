8d51315b9392e9d70d71de38b4ee42a6cf471708
jyothi_perla26 <jyothi.perla@everst.engineering>
2023-10-05 19:39:25 +0530

    test for the Get contacts button click

--- __tests__/Home.test.tsx
+++ __tests__/Home.test.tsx
@@ -1,27 +1,35 @@
 import 'react-native';
 import React from 'react';
 import HomeScreen from '../src/HomeScreen';
-import { render } from '@testing-library/react-native';
+import { render, fireEvent} from '@testing-library/react-native';
 
-
-// Note: import explicitly to use the types shiped with jest.
 import {it, test} from '@jest/globals';
-
-// Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
-it('renders correctly', () => {
-  renderer.create(<HomeScreen navigation={undefined}/>);
-});
 
-test('Should render the title of the Home Screen', () => {
-  const { getByText } = render(<HomeScreen navigation={undefined}/>);
-  const titleText = getByText('Welcome');
-  expect(titleText).toBeTruthy();
-});
 
-test('Should render the Get contacts button text on the screen', () => {
-  const { getByText } = render(<HomeScreen navigation={undefined}/>);
-  const getContactsText = getByText('Get contacts');
-  expect(getContactsText).toBeTruthy();
+describe('Home screen Component', () => {
+
+  test('renders correctly', () => {
+    renderer.create(<HomeScreen navigation={{}}/>);
+  });
+
+  test('Should render the title of the Home Screen', () => {
+    const { getByText } = render(<HomeScreen navigation={{}}/>);
+    const titleText = getByText('Welcome');
+    expect(titleText).toBeTruthy();
+  });
+
+  test('Should render the Get contacts button text on the screen', () => {
+    const { getByText } = render(<HomeScreen navigation={{}}/>);
+    const getContactsText = getByText('Get contacts');
+    expect(getContactsText).toBeTruthy();
+  });
+
+  test('handles Get contacts button', () => {
+    const { getByText } = render(<HomeScreen navigation={{}} />);
+    const getContactsButton = getByText('Get contacts');
+    fireEvent.press(getContactsButton);
+  });
+  
 });
